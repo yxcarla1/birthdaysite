3dbb9a4c364d52c93e19b897e2cd8e34e0bafa0c
yxcarla1 <businessyxcarla@gmail.com>
2024-11-09 20:27:39 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function openLetter() {
+    var letter = document.getElementById('expanded-letter');
+    letter.style.display = 'block';
+
+}
+function closeLetter() {
+    var letter = document.getElementById('expanded-letter');
+    letter.style.display = 'none';
+}
+
+window.alert('Happy Birthday, mon amour!');
+window.alert("Oh look, you've got mail!")
